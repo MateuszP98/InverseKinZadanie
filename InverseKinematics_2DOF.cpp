046c77afd3dd1e909db171a67c10f26dae5d5219
MateuszP98 <mateusz.p2013@gmail.com>
2023-04-02 14:56:06 +0200

    First version of inverse kinematics function

--- InverseKinematics_2DOF.cpp
+++ InverseKinematics_2DOF.cpp
@@ -9,7 +9,16 @@ void inverseKinematics(double x, double y, double& theta1, double& theta2) {
 
 	int L1 = 100;
 	int L2 = 150;
+    double r = sqrt(x*x + y*y);
+    // Calculate the angle between the first arm and the x-axis
+    theta1 = atan2(y, x) - acos((L1*L1 + r*r - L2*L2) / (2*L1*r));
 
+    // Calculate the angle between the second arm and the first arm
+    theta2 = acos((L1*L1 + L2*L2 - r*r) / (2*L1*L2));
+
+    // Convert angles to degrees
+    theta1 = theta1 * 180.0 / M_PI;
+    theta2 = theta2 * 180.0 / M_PI;
 
 
 }
