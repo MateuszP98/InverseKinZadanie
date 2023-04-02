48bce21d398155a5aae9ac7d654e5b960a0f159b
MateuszP98 <mateusz.p2013@gmail.com>
2023-04-02 20:08:08 +0200

    First version of inverse kinematics function -> without writing

--- InverseKinZadanie.cpp
+++ InverseKinZadanie.cpp
@@ -1,47 +1,112 @@
-﻿#include "InverseKinematics_2DOF.h"
+﻿//#include "InverseKinematics_2DOF.h"
+//#include <iostream>
+//#include <fstream>
+//#include <vector>
+//#include <cmath>
+//#include <chrono>
+//using namespace std;
+//
+//int main()
+//{
+//    double P0[2] = {400, 0};
+//    double P2[2] = {-400, 0};
+//
+//    // Define the coefficients of the quadratic equation
+//    const double a = -0.0025;
+//    const double b = 0.0;
+//    const double c = 400.0;
+//
+//    // Generate n_points points on the parabolic trajectory
+//    const long n_points = 2000000001;
+//    vector<double> x(n_points), y(n_points);
+//    const double dx = (P2[0] - P0[0]) / (n_points - 1);
+//    for (int i = 0; i < n_points; i++) {
+//        x[i] = P0[0] + i * dx;
+//        y[i] = a * x[i] * x[i] + b * x[i] + c;
+//    }
+//
+//    // Calculate joint angles for each point along the trajectory
+//    auto begin = chrono::high_resolution_clock::now();
+//    vector<pair<double, double>> joint_angles;
+//    for (int i = 0; i < n_points; i++) {
+//
+//        double theta1, theta2;
+//
+//        inverseKinematics(x[i], y[i], theta1, theta2);
+//        joint_angles.emplace_back(theta1, theta2);
+//    }
+//
+//    // Save joint angles to a file for plotting in Python
+////    ofstream outfile("joint_angles.txt");
+////    for (const auto& angles : joint_angles) {
+////        outfile << angles.first << " " << angles.second << endl;
+////    }
+////    outfile.close();
+//
+//    auto end = chrono::high_resolution_clock::now();
+//    auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
+//    printf("\nCzas realizacji algorytmu SEKWENCYJNEGO wynosi: %.0f [ms].\n", elapsed.count() * 1e-6);
+//    double seconds = elapsed.count() / 1e9;
+//    printf("\nCzas realizacji algorytmu SEKWENCYJNEGO wynosi: %.6f [s].\n", seconds);
+//
+//
+//    return 0;
+//}
+#include "InverseKinematics_2DOF.h"
 #include <iostream>
 #include <fstream>
-#include <vector>
 #include <cmath>
 #include <chrono>
 using namespace std;
 
 int main()
 {
-    double start_x = 0, start_y = 400.0;
-    double end_x = 400, end_y = 0;
-    long long num_points = 10000000000;
+    double P0[2] = {400, 0};
+    double P2[2] = {-400, 0};
 
-    double step_x = (end_x - start_x) / num_points;
-    double step_y = (end_y - start_y) / num_points;
+    // Define the coefficients of the quadratic equation
+    const double a = -0.0025;
+    const double b = 0.0;
+    const double c = 400.0;
+
+    // Generate n_points points on the parabolic trajectory
+    const long long n_points = 20000000001;
+    double x=0;
+    double y=0;
+    const double dx = (P2[0] - P0[0]) / (n_points - 1);
 
     // Calculate joint angles for each point along the trajectory
     auto begin = chrono::high_resolution_clock::now();
-    vector<pair<double, double>> joint_angles;
-    for (int i = 0; i < num_points; i++) {
+  //  double** joint_angles = new double*[n_points];
+    for (long long i = 0; i < n_points; i++) {
+    //    joint_angles[i] = new double[2];
 
-
-        double x = start_x + i * step_x;
-        double y = start_y + i * step_y;
         double theta1, theta2;
-
+        x = P0[0] + i * dx;
+        y = a * x * x + b * x + c;
         inverseKinematics(x, y, theta1, theta2);
-        joint_angles.emplace_back(theta1, theta2);
+        //joint_angles[i][0] = theta1;
+        //joint_angles[i][1] = theta2;
     }
+
+    // Save joint angles to a file for plotting in Python
+//    ofstream outfile("joint_angles.txt");
+//    for (int i = 0; i < n_points; i++) {
+//        outfile << joint_angles[i][0] << " " << joint_angles[i][1] << endl;
+//    }
+//    outfile.close();
+
     auto end = chrono::high_resolution_clock::now();
     auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
     printf("\nCzas realizacji algorytmu SEKWENCYJNEGO wynosi: %.0f [ms].\n", elapsed.count() * 1e-6);
     double seconds = elapsed.count() / 1e9;
     printf("\nCzas realizacji algorytmu SEKWENCYJNEGO wynosi: %.6f [s].\n", seconds);
 
-    // Save joint angles to a file for plotting in Python
-//    ofstream outfile("joint_angles.txt");
-//    for (const auto& angles : joint_angles) {
-//        outfile << angles.first << " " << angles.second << endl;
+    // Free dynamically allocated memory
+//    for (int i = 0; i < n_points; i++) {
+//        delete[] joint_angles[i];
 //    }
-//    outfile.close();
-
-
+//    delete[] joint_angles;
 
     return 0;
 }
