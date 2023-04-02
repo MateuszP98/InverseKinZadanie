f552a0980c54b5ad1c5af3b838fa9f8166ccbae7
MateuszP98 <mateusz.p2013@gmail.com>
2023-04-02 14:36:50 +0200

    Initial project

---
+++ InverseKinematics_2DOF.cpp
@@ -0,0 +1,15 @@
+#include "InverseKinematics_2DOF.h"
+
+
+void inverseKinematics(double x, double y, double& theta1, double& theta2) {
+
+	// L1 -> Length of the fiest elbow 
+	// L2 -> Length of the second elbow 
+	// r -> distance from the origin to the end effector
+
+	int L1 = 100;
+	int L2 = 150;
+
+
+
+}
---
+++ InverseKinematics_2DOF.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <cmath>
+
+using namespace std;
+
+#ifndef _InverseKinematics_2DOF_H
+#define _InverseKinematics_2DOF_H
+
+typedef enum { FALSE, TRUE } BOOLEAN;
+
+// Function prototypes
+
+void inverseKinematics(double x, double y, double& theta1, double& theta2);
+
+#endif
