6e95b0507ab9e0cb738889018e00b6f379a58fef
MateuszP98 <mateusz.p2013@gmail.com>
2023-04-02 17:46:53 +0200

    First version of inverse kinematics function experimental

--- InverseKinZadanie.cpp
+++ InverseKinZadanie.cpp
@@ -1,11 +1,47 @@
-﻿#include <iostream>
-
-
+﻿#include "InverseKinematics_2DOF.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <chrono>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+    double start_x = 0, start_y = 400.0;
+    double end_x = 400, end_y = 0;
+    long long num_points = 10000000000;
+
+    double step_x = (end_x - start_x) / num_points;
+    double step_y = (end_y - start_y) / num_points;
+
+    // Calculate joint angles for each point along the trajectory
+    auto begin = chrono::high_resolution_clock::now();
+    vector<pair<double, double>> joint_angles;
+    for (int i = 0; i < num_points; i++) {
 
 
+        double x = start_x + i * step_x;
+        double y = start_y + i * step_y;
+        double theta1, theta2;
+
+        inverseKinematics(x, y, theta1, theta2);
+        joint_angles.emplace_back(theta1, theta2);
+    }
+    auto end = chrono::high_resolution_clock::now();
+    auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
+    printf("\nCzas realizacji algorytmu SEKWENCYJNEGO wynosi: %.0f [ms].\n", elapsed.count() * 1e-6);
+    double seconds = elapsed.count() / 1e9;
+    printf("\nCzas realizacji algorytmu SEKWENCYJNEGO wynosi: %.6f [s].\n", seconds);
+
+    // Save joint angles to a file for plotting in Python
+//    ofstream outfile("joint_angles.txt");
+//    for (const auto& angles : joint_angles) {
+//        outfile << angles.first << " " << angles.second << endl;
+//    }
+//    outfile.close();
+
+
+
+    return 0;
 }
--- InverseKinematics_2DOF.cpp
+++ InverseKinematics_2DOF.cpp
@@ -7,14 +7,15 @@ void inverseKinematics(double x, double y, double& theta1, double& theta2) {
 	// L2 -> Length of the second elbow 
 	// r -> distance from the origin to the end effector
 
-	int L1 = 100;
-	int L2 = 150;
+	int L1 = 200;
+	int L2 = 200;
     double r = sqrt(x*x + y*y);
     // Calculate the angle between the first arm and the x-axis
-    theta1 = atan2(y, x) - acos((L1*L1 + r*r - L2*L2) / (2*L1*r));
-
+    //theta1 = atan2(y, x) - acos((L1*L1 + r*r - L2*L2) / (2*L1*r));
+    theta2 = acos((x*x+y*y-L1*L1-L2*L2)/(2*L1*L2));
+    theta1 = atan(y/x)-atan((L2*sin(theta2))/(L1+L2*cos(theta2)));
     // Calculate the angle between the second arm and the first arm
-    theta2 = acos((L1*L1 + L2*L2 - r*r) / (2*L1*L2));
+    //theta2 = acos((L1*L1 + L2*L2 - r*r) / (2*L1*L2));
 
     // Convert angles to degrees
     theta1 = theta1 * 180.0 / M_PI;
