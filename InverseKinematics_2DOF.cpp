03f4c413ef90182956c4478d10b95df7444a987e
MateuszP98 <mateusz.p2013@gmail.com>
2023-04-03 18:47:12 +0200

    Parabolic inverse kinematics solved with sequential solution

--- InverseKinematics_2DOF.cpp
+++ InverseKinematics_2DOF.cpp
@@ -1,15 +1,25 @@
 #include "InverseKinematics_2DOF.h"
 
 
-void inverseKinematics(double x, double y, double& theta1, double& theta2) {
+void inverseKinematics(double x, double y, double &theta1, double &theta2) {
 
-	// L1 -> Length of the fiest elbow 
-	// L2 -> Length of the second elbow 
-	// r -> distance from the origin to the end effector
+    // L1 -> Length of the fiest elbow
+    // L2 -> Length of the second elbow
+    // r -> distance from the origin to the end effector
 
-	int L1 = 100;
-	int L2 = 150;
+    int L1 = 200;
+    int L2 = 200;
+    double r = sqrt(x * x + y * y);
 
+    // Calculate the angle between the second arm and the first arm
+    theta2 = acos((x, x * x + y * y - L1 * L1 - L2 * L2) / (2 * L1 * L2));
+    // Calculate the angle between the first arm and the x-axis
+
+    theta1 = atan(y / x) - atan((L2 * sin(theta2)) / (L1 + L2 * cos(theta2)));
+
+    // Convert angles to degrees
+    theta1 = theta1 * 180.0 / M_PI;
+    theta2 = theta2 * 180.0 / M_PI;
 
 
 }
