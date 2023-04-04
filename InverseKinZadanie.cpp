40cbe6ce2fad6a81b58b8e36dee8a1b0c732a71c
MateuszP98 <mateusz.p2013@gmail.com>
2023-04-04 23:12:14 +0200

    First implementation of thread library for the inverse kinematics

--- InverseKinZadanie.cpp
+++ InverseKinZadanie.cpp
@@ -10,11 +10,34 @@
 using namespace std;
 mutex mtx;
 mutex m_thread;
-const long long n_points = 50000000;
-double* gtheta1 = new double[n_points];
-double* gtheta2 = new double[n_points];
 
-void calculateJointAngles(long long start, long long end, double dx, double P0, double a, double b, double c, double* theta1, double* theta2) {
+//Define global variables
+const long long n_points = 500;
+double* gtheta1 = (double*) malloc(n_points * sizeof(double));
+double* gtheta2 = (double*) malloc(n_points * sizeof(double));
+
+//Configuration variables
+bool WRITE_FILE = TRUE;
+
+void write_joint_angles(const std::string &filename, double *theta1, double *theta2, long long n_points) {
+    // Remove the file if it exists
+    remove(filename.c_str());
+
+    // Open the file for writing
+    std::ofstream outfile(filename, std::ios::app);
+
+    // Write the joint angles to the file
+    for (long long i = 0; i < n_points; i++) {
+        outfile << theta1[i] << " " << theta2[i] << std::endl;
+    }
+
+    // Close the file
+    outfile.close();
+}
+
+void
+calculateJointAngles(long long start, long long end, double dx, double P0, double a, double b, double c, double *theta1,
+                     double *theta2) {
     double x, y;
     for (long long i = start; i < end; i++) {
         x = P0 + i * dx;
@@ -22,13 +45,13 @@ void calculateJointAngles(long long start, long long end, double dx, double P0, 
         double t1, t2;
         inverseKinematics(x, y, t1, t2);
         // Store the joint angles in separate array
-        theta1[i-start] = t1;
-        theta2[i-start] = t2;
+        theta1[i - start] = t1;
+        theta2[i - start] = t2;
     }
     //mtx.lock();
     for (long long i = start; i < end; i++) {
-        gtheta1[i] += theta1[i-start];
-        gtheta2[i] += theta2[i-start];
+        gtheta1[i] += theta1[i - start];
+        gtheta2[i] += theta2[i - start];
     }
     //mtx.unlock();
 }
@@ -44,14 +67,14 @@ int main() {
     cin >> choice;
 
     // Define the start and end points of the parabolic trajectory
-    double P0[2] = {400, 0};
-    double P2[2] = {-400, 0};
+    const double P0[2] = {400, 0};
+    const double P2[2] = {-400, 0};
 
     // Define the coefficients of the quadratic equation
     const double a = -0.0025;
     const double b = 0.0;
     const double c = 400.0;
-    const int sd = 1;
+
     // Generate n_points points on the parabolic trajectory
 
     double x, y;
@@ -60,6 +83,7 @@ int main() {
     //Define number of threads and store them in an array
     const int n_threads = 50;
     thread threads[n_threads];
+
     if (choice == 1) {
         // Calculate joint angles for each point along the trajectory
 
@@ -74,53 +98,65 @@ int main() {
             gtheta2[i] += theta2;
 
         }
+
+        // calculate the execution time
+
         auto end = chrono::high_resolution_clock::now();
         auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
         double milliseconds = elapsed.count() * 1e-6;
         double seconds = elapsed.count() / 1e9;
+
         cout << "\nCzas realizacji algorytmu SEKWENCYJNEGO wynosi: " << fixed << setprecision(0) << milliseconds
              << " [ms].\n";
+
         cout << "\nCzas realizacji algorytmu SEKWENCYJNEGO wynosi: " << fixed << setprecision(6) << seconds
              << " [s].\n";
-//        remove("joint_anglesThreadSeq.txt");
-//        ofstream outfile("joint_anglesThreadSeq.txt", ios::app);
-//        for (long long i = 0; i < n_points; i++) {
-//            outfile << gtheta1[i] << " " << gtheta2[i] << endl;
-//        }
-//        outfile.close();
-        delete[] gtheta2;
-        delete[] gtheta1;
+
+        //write the values to a file
+        if (WRITE_FILE) {
+            write_joint_angles("joint_anglesThreadSeq.txt", gtheta1, gtheta2, n_points);
+        }
+        //free the gtheta arrays
+        free(gtheta1);
+        free(gtheta2);
 
     } else if (choice == 2) {
 
+        //Start timer
         auto begin = chrono::high_resolution_clock::now();
+
+        // Loop through the threads
         for (int i = 0; i < n_threads; i++) {
             const long long start = i * n_points / n_threads;
             const long long end = (i + 1) * n_points / n_threads;
-            double* local_theta1 = new double[(end-start)];
-            double* local_theta2 = new double[(end-start)];
-            threads[i] = thread(calculateJointAngles, start, end, dx, P0[0], a, b, c,local_theta1,local_theta2);
+            double* local_theta1 = (double*) malloc((end - start) * sizeof(double));
+            double* local_theta2 = (double*) malloc((end - start) * sizeof(double));
+            threads[i] = thread(calculateJointAngles, start, end, dx, P0[0], a, b, c, local_theta1, local_theta2);
         }
 
+        // Wait for threads to finish
         for (int i = 0; i < n_threads; i++) {
             threads[i].join();
         }
+
+        // calculate the execution time
         auto end = chrono::high_resolution_clock::now();
         auto elapsed = chrono::duration_cast<chrono::nanoseconds>(end - begin);
         double milliseconds = elapsed.count() * 1e-6;
         double seconds = elapsed.count() / 1e9;
+
         cout << "\nCzas realizacji algorytmu SEKWENCYJNEGO wynosi: " << fixed << setprecision(0) << milliseconds
+
              << " [ms].\n";
         cout << "\nCzas realizacji algorytmu SEKWENCYJNEGO wynosi: " << fixed << setprecision(6) << seconds
              << " [s].\n";
-//        remove("joint_anglesThreadNoRace.txt");
-//        ofstream outfile("joint_anglesThreadNoRace.txt", ios::app);
-//        for (long long i = 0; i < n_points; i++) {
-//            outfile << gtheta1[i] << " " << gtheta2[i] << endl;
-//        }
-//        outfile.close();
-        delete[] gtheta2;
-        delete[] gtheta1;
+        if (WRITE_FILE) {
+            //write the values to a file
+            write_joint_angles("joint_anglesThreadNoRace.txt", gtheta1, gtheta2, n_points);
+        }
+        //free the gtheta arrays
+        free(gtheta1);
+        free(gtheta2);
     }
 
 
